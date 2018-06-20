631c8e6b401721022ce67095ea37e0153460220a
emi.sasaki <emi.sasaki@rakuten.com>
2018-06-20 00:46:00 +0000

    Procfile

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -28,12 +28,23 @@ use \App\Item;
                 $item->url = $rws_item['Item']['itemUrl'];
                 $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
                 $items[] = $item;
-            }
-        }
-
+            }    
+        }        
+                
         return view('items.create', [
             'keyword' => $keyword,
             'items' => $items,
         ]);
+    }
+                
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
   }
